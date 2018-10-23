28037d3e6c4bc0c732dda23826f7ceae22e05a7b
David López Chica <Fasgort@gmail.com>
2018-10-23 17:59:54 +0200

    Code to create a sqlite database with the needed tables.

--- main/dbserver.py
+++ main/dbserver.py
@@ -1,18 +1,34 @@
-import os, sys
-import PIL
-from PIL import Image
-from time import sleep
-import serial
-ser = serial.Serial('COM3', 115200) # Establish the connection on a specific port
-while True:
-     #im1 = PIL.Image.frombytes("L",(160,120),ser.read(19200))
-     #im1.save("D:\\Fasgort\\Desktop\\TFM\\FPS_dbserver\\main\\fingerprint_test.bmp")
-     #ser.read(2);
-     
-     im2 = PIL.Image.frombytes("L",(232,139),ser.read(32248))
-     #im2 = PIL.Image.frombytes("L",(232,224),ser.read(52116))
-     im2.save("D:\\Fasgort\\Desktop\\TFM\\FPS_dbserver\\main\\fingerprint_test2.bmp")
-     ser.read(2);
+import sqlite3
+ 
+conn = sqlite3.connect("fingerprint.db")
+ 
+cursor = conn.cursor()
+ 
+### creating tables
+# ID_user, template_data, signup_date expressed in UNIX time
+cursor.execute("""CREATE TABLE fingerprint
+                  (ID_user integer, fingerprint_data blob, fingerprint_scan_date int) 
+               """)
 
-     ser.close();
-     sys.exit()
+# ID_user, user_name, group
+# group works as linux privileges work, in this case:
+# 0111 means first number is admin, second is group 1, third is group 2, fourth is group 3...
+# So this user is not admin, but is a user of the three first groups
+cursor.execute("""CREATE TABLE user
+                  (ID_user integer, user_name text, group int) 
+               """)
+
+# ID_FPS, signup_date (unix time), location, group_access
+# group_access allows groups to use the FPS or not. If a user is member of a group
+# with access 1, he will be allowed access.
+# As special use, a group with access 2 won't be allowed access not matter what,
+# even if the user is in a different group with acess 1
+cursor.execute("""CREATE TABLE fps
+                  (ID_FPS integer, signup_date int, location text, group_access int) 
+               """)
+
+# ID_log, ID_user, ID_FPS, date, access_granted
+# Will save access_log, including user and FPS used, date, and if access was allowed or not
+cursor.execute("""CREATE TABLE log
+                  (ID_log int, ID_user int, ID_FPS int, date int, access_granted int) 
+               """)
