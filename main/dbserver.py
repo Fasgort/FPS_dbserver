c574435793b2ea658eacd304e1dd0061c78ae894
David López Chica <Fasgort@gmail.com>
2019-01-05 20:07:03 +0100

    Reworked full DB sync method (partial is still not done). Changed again the number of hashes sent at once to 8 per packet, to avoid spending too much memory in the client device. Additional debug methods implemented.

--- main/dbserver.py
+++ main/dbserver.py
@@ -189,21 +189,17 @@ def scanner_listener(data, addr, s):
 		# Delete list
 		if len(deletion_hashes) > 0:
 			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 222, len(deletion_hashes)])), addr)
-			time.sleep(1)
 			print(bytes(deletion_hashes))
 			s.sendto(encrypt_bytes(bytes(deletion_hashes)), addr)
-			time.sleep(1)
 
 		# Addition list
 		if len(ddbb_hashes) > 0:
 			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 173, len(ddbb_hashes)])), addr)
-			time.sleep(1)
 			addition_hashes = bytearray()
 			for fingerprint in ddbb_hashes:
 				addition_hashes.extend(fingerprint.to_bytes(4, byteorder='big'))
 			print(addition_hashes)
 			s.sendto(encrypt_bytes(addition_hashes), addr)
-			time.sleep(1)
 
 		# End sync process
 		s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 13, 0])), addr)
@@ -216,15 +212,56 @@ def scanner_listener(data, addr, s):
 			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 13, 0])), addr)
 			return
 
-		s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 173, num_additions])), addr)
-		time.sleep(1)
-
 		cursor.execute("SELECT fingerprint_hash FROM users")
 		hash_data = bytearray()
 		for row in cursor:
 			hash_data += bytearray(row[0].to_bytes(4, byteorder="big"))
-		s.sendto(encrypt_bytes(hash_data), addr)
-		time.sleep(1)
+
+		num_packets = num_additions//8
+		if num_additions % 8 != 0:
+			num_packets += 1
+
+		for packet in range(num_packets):
+			if packet == num_packets-1:
+				num_fingerprints = num_additions % 8
+			else:
+				num_fingerprints = 8
+
+			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 173, num_fingerprints])), addr)
+			s.sendto(encrypt_bytes(bytes(hash_data[packet*32:packet*32 + num_fingerprints*4])), addr)
+
+			for i in range(num_fingerprints):
+
+				try:
+					# receive data from client (data, addr)
+					d = s.recvfrom(1024)
+				except socket.timeout:
+					return
+
+				data = d[0]
+				addr = d[1]
+
+				if not data or len(data) < 28:
+					return
+
+				try:
+					# Decrypt the packet
+					nonce = data[0:12]
+					message_withtag = data[12:]
+					data = aesgcm.decrypt(nonce, message_withtag, None)
+				except Exception: # All the possible exceptions should be safely ignored
+					print("Failed decryption at DDBB sync.")
+					continue
+
+				print(data.hex())
+
+				if len(data) >= 5 and data[0] == 1 and data[4] == 48:
+					fingerprint_hash_requested = int.from_bytes([data[5], data[6], data[7], data[8]], byteorder='big')
+					cursor.execute("SELECT fingerprint_data FROM users WHERE fingerprint_hash=?", (str(fingerprint_hash_requested),))
+					fingerprint_data = cursor.fetchone()[0]
+					s.sendto(encrypt_bytes(fingerprint_data), addr)
+
+				time.sleep(1) # Additional sleep time to allow the FPS to finish
 
 		# End sync process
 		s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 13, 0])), addr)
@@ -248,10 +285,12 @@ def code_interpreter(argument):
     return func
 
 def encrypt_bytes(data):
+	print(data.hex())
+	time.sleep(1) # Require testing for better timings
 	nonce = os.urandom(12)
 	message_withtag = aesgcm.encrypt(nonce, data, None)
 	return nonce+message_withtag
-	
+
 # Datagram (udp) socket
 try :
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -288,14 +327,14 @@ while 1:
 		# Decrypt the packet
 		nonce = data[0:12]
 		message_withtag = data[12:]
-		data = aesgcm.decrypt(nonce, message_withtag, None) # TO-DO fail check
+		data = aesgcm.decrypt(nonce, message_withtag, None)
 	except Exception: # All the possible exceptions should be safely ignored
 		print("Failed decryption at initial communication.")
 		continue
 
 	print(data.hex())
 
-	if data[0] == 1 and len(data) >= 5: # First byte has always to be 0x01
+	if len(data) >= 5 and data[0] == 1: # First byte has always to be 0x01
 		listener = code_interpreter(data[1])
 		if listener == 'error':
 			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 238])), addr) # 0x01 0xDB 0x00 0x01 0xEE == 1 219 0 1 238 (EE error)
