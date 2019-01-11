b6d9a9f4efe75c6b65b4624fe74b57c00fa80623
David López Chica <Fasgort@gmail.com>
2019-01-11 22:59:21 +0100

    Partial sync implemented.

--- main/dbserver.py
+++ main/dbserver.py
@@ -129,8 +129,8 @@ def scanner_listener(data, addr, s):
 	elif data[4] == 93: # 0x5D == 93(Requesting partial DDBB download)
 
 		num_enrolled = data[5]
-		num_packets_sent = num_enrolled//128
-		if num_enrolled%128 > 0:
+		num_packets_sent = num_enrolled//8
+		if num_enrolled%8 > 0:
 			num_packets_sent += 1
 
 		cursor.execute("SELECT fingerprint_hash FROM users")
@@ -168,11 +168,11 @@ def scanner_listener(data, addr, s):
 			print(data.hex())
 
 			if packet == num_packets_sent-1:
-				num_fingerprints = num_enrolled%128
+				num_fingerprints = num_enrolled%8
 				if num_fingerprints == 0:
-					num_fingerprints = 128
+					num_fingerprints = 8
 			else:
-				num_fingerprints = 128
+				num_fingerprints = 8
 
 			for fingerprint in range(num_fingerprints):
 				fingerprint_hash = int.from_bytes([data[fingerprint*4], data[fingerprint*4 + 1], data[fingerprint*4 + 2], data[fingerprint*4 + 3]], byteorder='big')
@@ -180,28 +180,75 @@ def scanner_listener(data, addr, s):
 					ddbb_hashes.remove(fingerprint_hash)
 					print("Exists: " + str(fingerprint_hash))
 				else:
-					deletion_hashes.append(packet*128 + fingerprint) # Gives the fingerprint_num to delete, instead of the hash
-					print("Remove fingerprint "+ str(packet*128 + fingerprint) + ": " + str(fingerprint_hash))
+					deletion_hashes.append(packet*8 + fingerprint) # Gives the fingerprint_num to delete, instead of the hash
+					print("Remove fingerprint "+ str(packet*8 + fingerprint) + ": " + str(fingerprint_hash))
 
 		print("Update list...")
 		print(ddbb_hashes)
 
 		# Delete list
 		if len(deletion_hashes) > 0:
-			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 222, len(deletion_hashes)])), addr)
+			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 222, len(deletion_hashes)])), addr) # Can delete 32 at one time
 			print(bytes(deletion_hashes))
 			s.sendto(encrypt_bytes(bytes(deletion_hashes)), addr)
+			time.sleep(2) # Time needed is based depending on how many deletions are queued, requires TESTING
+
+		# End sync delete process
+		s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 13, 0])), addr)
 
 		# Addition list
 		if len(ddbb_hashes) > 0:
-			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 173, len(ddbb_hashes)])), addr)
 			addition_hashes = bytearray()
 			for fingerprint in ddbb_hashes:
-				addition_hashes.extend(fingerprint.to_bytes(4, byteorder='big'))
-			print(addition_hashes)
-			s.sendto(encrypt_bytes(addition_hashes), addr)
+				addition_hashes += bytearray(fingerprint.to_bytes(4, byteorder='big'))
 
-		# End sync process
+		num_packets = len(ddbb_hashes)//8
+		if len(ddbb_hashes) % 8 != 0:
+			num_packets += 1
+
+		for packet in range(num_packets):
+			if packet == num_packets-1:
+				num_fingerprints = len(ddbb_hashes) % 8
+			else:
+				num_fingerprints = 8
+
+			s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 173, num_fingerprints])), addr)
+			s.sendto(encrypt_bytes(bytes(addition_hashes[packet*32:packet*32 + num_fingerprints*4])), addr)
+
+			for i in range(num_fingerprints):
+
+				try:
+					# receive data from client (data, addr)
+					d = s.recvfrom(1024)
+				except socket.timeout:
+					return
+
+				data = d[0]
+				addr = d[1]
+
+				if not data or len(data) < 28:
+					return
+
+				try:
+					# Decrypt the packet
+					nonce = data[0:12]
+					message_withtag = data[12:]
+					data = aesgcm.decrypt(nonce, message_withtag, None)
+				except Exception: # All the possible exceptions should be safely ignored
+					print("Failed decryption at DDBB sync.")
+					continue
+
+				print(data.hex())
+
+				if len(data) >= 5 and data[0] == 1 and data[4] == 48:
+					fingerprint_hash_requested = int.from_bytes([data[5], data[6], data[7], data[8]], byteorder='big')
+					cursor.execute("SELECT fingerprint_data FROM users WHERE fingerprint_hash=?", (str(fingerprint_hash_requested),))
+					fingerprint_data = cursor.fetchone()[0]
+					s.sendto(encrypt_bytes(fingerprint_data), addr)
+
+				time.sleep(1) # Additional sleep time to allow the FPS to finish
+
+		# End sync add process
 		s.sendto(encrypt_bytes(bytes([1, 219, 0, 1, 13, 0])), addr)
 		return
 
