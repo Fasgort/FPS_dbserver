80c5ee40c63ebe1fb7ccb35e7f646974ed24d882
David López Chica <Fasgort@gmail.com>
2019-02-18 21:59:14 +0100

    Added DDBB tables to the admin panel of Django.

--- main/FPS_DDBB/models.py
+++ main/FPS_DDBB/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 def validate_maxusers(value):
     if value >= 200: # change to 3000 if using GT-521F52
@@ -16,6 +17,9 @@ class User(models.Model):
 	fingerprint_hash = models.IntegerField()
 	fingerprint_scan_date = models.DateTimeField('Date of last scan')
 	
+	def __str__(self):
+		return self.user_name + " (ID: " + str(self.ID_user) + ")"
+	
 	class Meta:
 		db_table = 'users'
 
@@ -25,6 +29,9 @@ class FPS(models.Model):
 	signup_date = models.DateTimeField('Date of registration')
 	location = models.CharField(max_length=200)
 	group_access = models.IntegerField()
+	
+	def __str__(self):
+		return self.location + " (ID: " + str(self.ID_FPS) + ")"
 	
 	class Meta:
 		db_table = 'fps'
@@ -36,6 +43,12 @@ class Log(models.Model):
 	ID_FPS = models.ForeignKey(FPS, on_delete=models.CASCADE, db_column='ID_FPS')
 	date = models.DateTimeField('Scan date')
 	access_granted = models.IntegerField()
+
+	def __str__(self):
+		if self.access_granted == 1:
+			return "Log #" + str(self.ID_log) + ":  user ID " + str(self.ID_user.ID_user) + " accessed FPS #" + str(self.ID_FPS.ID_FPS) + " and was GRANTED access. Date: " + self.date.astimezone(tz=timezone.get_current_timezone()).ctime()
+		else:
+			return "Log #" + str(self.ID_log) + ":  user ID " + str(self.ID_user.ID_user) + " accessed FPS #" + str(self.ID_FPS.ID_FPS) + " and was DENIED access. Date: " + self.date.astimezone(tz=timezone.get_current_timezone()).ctime()
 
 	class Meta:
 		db_table = 'log'
